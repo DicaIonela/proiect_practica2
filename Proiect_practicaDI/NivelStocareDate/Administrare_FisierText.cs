c5302790e89cdbd98e6f459fdad07d22adef61ff
ioneladd <164873125+ioneladd@users.noreply.github.com>
2024-07-23 03:11:04 -0700

    Add files via upload

---
+++ Proiect_practicaDI/NivelStocareDate/Administrare_FisierText.cs
@@ -0,0 +1,93 @@
+﻿using LibrarieClase;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NivelStocareDate
+{
+    public class Administrare_FisierText
+    {
+        private const int NR_MAX = 50;
+        private string numeFisier;
+
+
+
+        /// CONSTRUCTOR LINII FISIER
+        public Administrare_FisierText(string numeFisier)
+        {
+            this.numeFisier= numeFisier;
+            Stream streamFisier = File.Open(numeFisier, FileMode.OpenOrCreate);
+            streamFisier.Close();
+        }
+
+        ///ADAUGARE UTILIZATOR IN FISIER 
+        public void AddUtilizator(Utilizator utilizator)
+        {
+            using (StreamWriter streamwriterFisierText = new StreamWriter(numeFisier, true))
+            {
+                streamwriterFisierText.WriteLine(utilizator.Conversie_PentruFisier());
+            }
+        }
+
+        ///VERIFICA EXISTENTA UTILIZATORULUI IN FISIER PENTRU A EVITA SCRIEREA UNEI COPII
+        public bool UtilizatorExista(string nume, string numar)
+        {
+            if (!File.Exists(numeFisier))
+                return false;
+
+            using (StreamReader streamReader = new StreamReader(numeFisier))
+            {
+                string linie;
+                while ((linie = streamReader.ReadLine()) != null)
+                {
+                    Utilizator utilizator = new Utilizator(linie);
+                    if (utilizator.Nume == nume && utilizator.Numar == numar)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        ///STOCHEAZA UTILIZATORII DIN FISIER INTR-UN TABLOU DE OBIECTE 
+        public Utilizator[] GetUtilizatori(out int nrUtilizatori)
+        {
+            Utilizator[] utilizatori = new Utilizator[NR_MAX];
+            using (StreamReader streamrdr=new StreamReader(numeFisier))
+            {
+                string linieFisier;
+                nrUtilizatori = 0;
+                while ((linieFisier = streamrdr.ReadLine()) != null)
+                {
+                    utilizatori[nrUtilizatori++]=new Utilizator(linieFisier);
+
+                }
+                Array.Resize(ref utilizatori, nrUtilizatori);
+
+            }
+            return utilizatori;
+
+        }
+        
+        public Utilizator[] CautaUtilizator(string criteriu)
+        {
+            int nrUtilizatori = 0;
+            Utilizator[] utilizatori = GetUtilizatori(out nrUtilizatori);
+            List <Utilizator> utilizatorigasiti= new List<Utilizator>();
+            foreach (Utilizator utilizator in utilizatori)
+            {
+                if (utilizator != null && utilizator.Nume != null && utilizator.Nume.Contains(criteriu) )
+                {
+                    utilizatorigasiti.Add(utilizator);
+                }
+            }
+            return utilizatorigasiti.ToArray();
+        }
+    }
+    }
+
---
+++ Proiect_practicaDI/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using NivelStocareDate;
+using System.Configuration;
+using LibrarieClase;
+
+namespace Proiect_practicaDI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string numeFisier = ConfigurationManager.AppSettings["NumeFisier"];
+            string locatieFisierSolutie = Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+            string caleCompletaFisier = locatieFisierSolutie + "\\Proiect_practicaDI\\" + numeFisier;
+            Administrare_FisierText admin=new Administrare_FisierText(caleCompletaFisier);
+            string optiune;
+            Utilizator utilizatornou= new Utilizator();
+            Utilizator utilizator = new Utilizator("Ion Popescu", "0737774955", "00::11::22::33::44::55");
+            
+
+
+            //MENIU
+            Console.WriteLine("MENIU:");
+            Console.WriteLine("C. Citire utilizator");
+            Console.WriteLine("S. Salvare utilizator");
+            Console.WriteLine("A. Afisare utilizatori din fisier.");
+            do
+            {
+                Console.WriteLine("\nIntrodu optiunea dorita:");
+                optiune = Console.ReadLine();
+                switch (optiune)
+                {
+                    case "C":
+                        utilizatornou = CitireUtilizatorTastatura();
+                        break;
+                    case "S":
+                        
+                        /// VERIFICARE NUMAR DE TELEFON VALID 
+                       
+                        if (utilizatornou.Numar.Length != 10)
+                            Console.WriteLine("Numarul utilizatorului este invalid.");
+                        else
+                        {
+                            ///DACA UTILIZATORUL NU EXISTA IN FISIER, VA FI ADAUGAT, IAR DACA EXISTA, SE VA AFISA UN MESAJ CORSPUNZATOR
+                            
+                            if (!admin.UtilizatorExista(utilizatornou.Nume, utilizatornou.Numar))
+                            {
+                                admin.AddUtilizator(utilizatornou);
+                                Console.WriteLine("Utilizatorul a fost adaugat cu succes.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Utilizatorul este deja in fisier.");
+                            }
+                        }
+                        break;
+                    case "A":
+                        ///CREAM UN TABLOU DE OBIECTE
+                        
+                        Utilizator[] utilizatori = admin.GetUtilizatori(out int nrUtilizatori);
+                        AfisareUtilizatori(utilizatori, nrUtilizatori);
+                        break;
+                }
+
+            } while (true);
+
+            
+            Console.ReadLine();//FOLOSIT PENTRU A FI PUS PROGRAMUL IN ASTEPTARE; EVITA INCHIDEREA APLICATIEI
+        }
+        public static void AfisareUtilizatori(Utilizator[]utilizatori, int nrUtilizatori)
+        {
+            Console.WriteLine("Utilizatorii salvati in fisier sunt:");
+
+            /// SE PARCURGE TABLOUL DE OBIECTE SI SE AFISEAZA INFORMATIILE IN FORMATUL CORESPUNZATOR 
+            
+            for (int contor=0; contor<nrUtilizatori; contor++)
+            {
+                string infoLocuri = utilizatori[contor].Info();
+                Console.WriteLine(infoLocuri);
+             }
+        }
+        public static Utilizator CitireUtilizatorTastatura()
+        {
+            Console.WriteLine("Introduceti datele utilizatorului:");
+            Console.WriteLine("Nume:");
+            string nume= Console.ReadLine();
+
+            Console.WriteLine("Numar:");
+            string numar = Console.ReadLine();
+
+            Console.WriteLine("Adresa MAC:");
+            string adresamac = Console.ReadLine();
+
+            Utilizator utilizator = new Utilizator(nume, numar, adresamac);
+            return utilizator;
+        }
+    }
+}
